b4a4669fda4696f7b1bd7e78f7a3771033ab6e72
aadicoder1 <aadityasahu1203@gmail.com>
2026-06-29 13:19:07 +0530

    .

--- backend/api/app/utils/security.py
+++ backend/api/app/utils/security.py
@@ -2,8 +2,8 @@ from api.app.config import pwd_context
 
 def hash_password(password: str) -> str:
     """Hash password for storage"""
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verify user password"""
-    return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password[:72], hashed_password)
